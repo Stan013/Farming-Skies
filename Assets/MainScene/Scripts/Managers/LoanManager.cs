e62078c3da5e998755e4af350a1af2d6faaaec75
Stan013 <stanvdakker2000@gmail.com>
2026-06-29 23:13:58 +0200

    Loan company assets

--- Assets/MainScene/Scripts/Managers/LoanManager.cs
+++ Assets/MainScene/Scripts/Managers/LoanManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class LoanManager : MonoBehaviour
 {
     [Header("Loan variables")]
+    [SerializeField] private string[] farmNames;
+    [SerializeField] private Sprite[] farmIcons;
     
     public void GenerateLoanOptions()
     {
